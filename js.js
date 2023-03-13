5580ba3d2c3f862600bf6bc198be6539e9052ccf
r03n3 <r03n3@web.de>
2023-03-13 17:03:31 +0100

    feat: centralization of the grid. Mouseovercolorchange added.

--- js.js
+++ js.js
@@ -1,13 +1,13 @@
 function grid() {
     const targetElement = document.getElementById("container");
-  
+
     for (let i = 0; i < 16; i++) {
       const divElementY = document.createElement("div");
       divElementY.style.display = "flex";
       divElementY.style.height = "calc(100% / 16)";
       divElementY.style.width = "calc(100% / 16)";
-      divElementY.style.minHeight ="5px";
-      divElementY.style.minWidth ="5px";
+      divElementY.style.minHeight ="10px";
+      divElementY.style.minWidth ="10px";
       //divElementY.style.border = "1px solid gray";
   
       for (let j = 0; j < 16; j++) {
@@ -15,8 +15,11 @@ function grid() {
         divElementX.style.height = "100%";
         divElementX.style.width = "100%";
         divElementX.style.border = "1px solid gray";
-        divElementX.style.minHeight ="5px";
-        divElementX.style.minWidth ="5px";  
+        divElementX.style.minHeight ="10px";
+        divElementX.style.minWidth ="10px";
+        divElementX.addEventListener("mouseover", function(){
+            divElementX.style.backgroundColor ="black";
+        });  
         divElementY.appendChild(divElementX);
       }
   
@@ -24,3 +27,4 @@ function grid() {
     }
   }
   
+  grid();
