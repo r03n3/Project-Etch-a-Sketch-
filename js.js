bf841b150db4863be2390197f07ae2968445f4cf
r03n3 <r03n3@web.de>
2023-03-14 13:25:12 +0100

    bugfix: corrected cellsize calculation and reading of the cellsize

--- js.js
+++ js.js
@@ -6,24 +6,28 @@ function buttonInput(){
         let tableContainer = document.getElementById("container");
         tableContainer.innerHTML= "";
         size = prompt("Please enter the wished size for the grid", 16);
+        while (size<=0 || size >200){
+          size = prompt("Please enter a value between 1 and 200");
+        }
         grid(size);
     });
 }
 
 function grid(size) {
     const targetElement = document.getElementById("container");
-    let cellSize = (400 - 2*size) / size; // size of a single gridcell based on the sizeparameter
-
+    let cellSize = 400/size; 
     for (let i = 0; i < size; i++) {
       const divElementY = document.createElement("div");
       divElementY.style.display = "flex";
-      divElementY.style.height = "${cellSize}px";
+      divElementY.style.height = `${cellSize}px`;
+      divElementY
       divElementY.style.width = "400px"
+
   
       for (let j = 0; j < size; j++) {
         const divElementX = document.createElement("div");
-        divElementX.style.height = "${cellSize}px";
-        divElementX.style.width = "${cellSize}px";
+        divElementX.style.height = `${cellSize}px`;
+        divElementX.style.width = `${cellSize}px`;
         divElementX.addEventListener("mouseover", function(){
             divElementX.style.backgroundColor ="black";
         });  
